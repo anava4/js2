37bd964894ba542018a55f46180bebc66131238f
anava4 <anava4@mail.greenriver.edu>
2019-04-10 15:46:49 -0400

    Edited else if statements

--- JSExercise2.js
+++ JSExercise2.js
@@ -1,24 +1,23 @@
 for (let x=1; x <= 100; x++)
     {
-        if (x % 3 && x % 5)
+        if (x % 3 === 0 && x % 5 === 0)
         {
             console.log("Hee Haw!" + "<br>");
         }
+        else if(x % 3 === 0)
+        {
+            console.log("Hee!" + "<br>");
+        }
+        else if(x % 5 === 0)
+        {
+            console.log("Haw!" + "<br>");
+        }
         else
             {
-            if (x % 3 === 0)
-            {
-                console.log("Hee!" + "<br>");
-            }
-            if (x % 5 === 0)
-            {
-                console.log("Haw!" + "<br>");
-            }
-            else{
                 console.log(x);
             }
 
-        }
+
 
 
     }
